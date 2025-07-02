5675c2bc39d74c29349087db98e34349f8784f25
Kr1s Ng <piq.k0olz@gmail.com>
2025-07-02 22:08:59 +0200

    cpp04

---
+++ cpp04/intro/polymorphism/polymorphism.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   polymorphism.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 14:21:16 by tat-nguy          #+#    #+#             */
+/*   Updated: 2025/07/02 14:47:43 by tat-nguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+class Character
+{
+	public:
+		void	sayHello(std::string const &target);
+
+};
+
+class Warrior: public Character
+{
+	public:
+		void	sayHello(std::string const &target); // re-define by function overwriting
+		
+};
+
+class Cat
+{
+
+};
+
+void	Character::sayHello(std::string const &target)
+{
+	std::cout << "Hello " << target << " !" << std::endl;
+}
+
+void	Warrior::sayHello(std::string const &target) // for overwrite 
+{
+	std::cout << "Fuck off " << target << ", I don't like you!" << std::endl;
+}
+
+int	main(void)
+{
+	// it's ok bcs obviously Warrior is a Warrior
+	Warrior		*a = new Warrior();
+
+	// it's also ok bcs Warrior is a Character
+	Character	*b = new Warrior();
+
+	// but it'd not be ok bcs Character is NOT Warrior
+	// Warrior		*c = new Character();
+	
+	a->sayHello("students");
+
+	b->sayHello("Students");
+	
+	return (0);
+}
