26678a0d772acd7a8999867f5fdcb389bccb10e9
Kr1s Ng <piq.k0olz@gmail.com>
2025-08-11 09:51:08 +0200

    cpp08

--- cpp08/exercises/ex00/easyfind.hpp
+++ cpp08/exercises/ex00/easyfind.hpp
@@ -6,25 +6,39 @@
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 10:01:24 by tat-nguy          #+#    #+#             */
-/*   Updated: 2025/08/08 10:37:46 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2025/08/09 08:57:12 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _EASY_
 # define _EASY_
 
-# include <cstddef>
-# include <stdexcept>
+# include <algorithm>   //std::find
+# include <stdexcept>   //std::runtime_error
+
+// iterators, like pointer, are used to access and iterate through elements of data structures
+// by "pointing" them.
+// data structures that support iterators: vector, list, deque, map, set
+// (stacks and queues do not)
 
 template <typename T>
-T easyfind(T container, int n)
+// using typename here for indicating that a dependent name is a type
+// helps the compiler knows for sure that T::iterator is a type !!
+typename T::iterator    easyfind(T &container, int value)   //modifiable
 {
-    for (; container != NULL; ++container)
-        if (*container == n)
-            return (*container);
-    throw std::runtime_error("runtime");
-    return (NULL);
+    typename T::iterator    it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())  // come after the last elem of container
+        throw (std::runtime_error("Value not found"));
+    return (it);
 }
 
+template <typename T>
+typename T::const_iterator   easyfind(T const &container, int value) //read-only
+{
+    typename T::const_iterator   it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw (std::runtime_error("Value not found"));
+    return (it);
+}
 
 #endif
--- cpp08/exercises/ex00/main.cpp
+++ cpp08/exercises/ex00/main.cpp
@@ -6,25 +6,59 @@
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 10:09:30 by tat-nguy          #+#    #+#             */
-/*   Updated: 2025/08/08 10:27:35 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2025/08/08 19:20:55 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// the difference between iterator and const_iterator are:
+// - iterator: we can read and modify the value it points to, example:
+//	std::list<int>::iterator it = lst.begin();
+//	*it = 99;	//=> allowed
+// - but with std::list<int>::const_iterator ic = lst.begin();
+//	*ic = 99; // => not allowed as the value of elem is const.
+//	however, ++ic; //=> allowed as we're moving the iterator, not modifying the value of elem
+
 #include "easyfind.hpp"
 #include <iostream>
+#include <vector>
+#include <list>
 
 int	main(void)
 {
-	int	arr[10];
-	
-	for (int i = 0; i < 10; ++i)
-		arr[i] = i;
-	
-	int	needle = -22;
-	if (easyfind(arr, needle))
-		std::cout << "arr contains " << needle << std::endl;
-	else
-		std::cout << "arr does not contains " << needle << std::endl;
+	// vector of int
+	std::vector<int>	vect;
+	vect.push_back(2);
+	vect.push_back(4);
+	vect.push_back(42);
+	vect.push_back(1337);
+	try
+	{
+		std::vector<int>::iterator	it1 = easyfind(vect, 1337);
+		std::cout << "Vect: Found " << *it1 << std::endl;
+		
+		std::vector<int>::iterator	it2 = easyfind(vect, 43);
+		std::cout << "Vect: Found " << *it2 << std::endl;
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << "Vect: " << e.what() << std::endl;
+	}
+
+	// list of int
+	int const	arr[] = {2, 4, 42, 1337};
+	std::list<int> const	lst(arr, arr + 4);
+	try
+	{
+		std::list<int>::const_iterator il1 = easyfind(lst, 1337);
+		std::cout << "Const lst: Found " << *il1 << std::endl;
+
+		std::list<int>::const_iterator il2 = easyfind(lst, 43);
+		std::cout << "Const lst: Found " << *il2 << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Const lst: " << e.what() << std::endl;
+	}
 	
 	return (0);
 }
